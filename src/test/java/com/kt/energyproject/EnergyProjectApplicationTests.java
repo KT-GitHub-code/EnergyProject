97c0fec24a19eb7009dfcfe5588bc0d511127cff
KT <krakovszkyt@gmail.com>
2024-03-12 23:12:21 +0100

    initial commit

---
+++ src/test/java/com/kt/energyproject/EnergyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.energyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
