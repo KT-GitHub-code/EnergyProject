22072b2dec0f1c797dfaab28c1c597ea2622c69b
KT <krakovszkyt@gmail.com>
2024-03-16 21:04:33 +0100

    add first steps of simulation

--- src/main/java/com/kt/energyproject/EnergyProjectApplication.java
+++ src/main/java/com/kt/energyproject/EnergyProjectApplication.java
@@ -1,13 +1,26 @@
 package com.kt.energyproject;
 
+import com.kt.energyproject.types.powerplants.SolarPowerTower;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// TEMP - exclude the datasource
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EnergyProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EnergyProjectApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EnergyProjectApplication.class, args);
+
+        startSimulation(context);
+
+    }
+
+    private static void startSimulation(ApplicationContext context) {
+        SolarPowerTower powerPlant = context.getBean(SolarPowerTower.class);
+
+        powerPlant.start();
     }
 
 }
--- src/main/java/com/kt/energyproject/types/powerplants/SolarPowerTower.java
+++ src/main/java/com/kt/energyproject/types/powerplants/SolarPowerTower.java
@@ -1,6 +1,27 @@
 package com.kt.energyproject.types.powerplants;
 
-public class SolarFurnace extends SolarPowerPlant implements ConcentratedSolarPower{
+import com.kt.energyproject.types.generators.Generator;
+import com.kt.energyproject.types.turbines.SteamTurbine;
+import com.kt.energyproject.types.turbines.factory.SteamTurbineFactory;
 import org.springframework.stereotype.Component;
+
 @Component
+public class SolarPowerTower extends SolarPowerPlant implements ConcentratedSolarPower {
+
+    private final SteamTurbineFactory turbineFactory;
+    private final Generator generator;
+
+    public SolarPowerTower(SteamTurbineFactory turbineFactory,
+                           Generator generator) {
+        this.turbineFactory = turbineFactory;
+        this.generator = generator;
+    }
+
+    public void start() {
+        super.start();
+        SteamTurbine turbine = (SteamTurbine)turbineFactory.createTurbine();
+        turbine.turn();
+        generator.generateElectricity();
+    }
+
 }
