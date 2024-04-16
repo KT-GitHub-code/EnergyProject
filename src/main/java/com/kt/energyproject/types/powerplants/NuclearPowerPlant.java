e786c10667ed393d069024627179ab661b040ded
KT <krakovszkyt@gmail.com>
2024-04-16 23:12:28 +0200

    remove unwanted constructor

--- src/main/java/com/kt/energyproject/types/powerplants/NuclearPowerPlant.java
+++ src/main/java/com/kt/energyproject/types/powerplants/NuclearPowerPlant.java
@@ -1,19 +1,12 @@
 package com.kt.energyproject.types.powerplants;
 
 import com.kt.energyproject.common.PowerPlantBase;
-import com.kt.energyproject.types.turbines.factory.SteamTurbineFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class NuclearPowerPlant extends PowerPlantBase {
 
     private static final Logger logger = LoggerFactory.getLogger(NuclearPowerPlant.class);
-
-    private final SteamTurbineFactory turbineFactory;
-
-    protected NuclearPowerPlant(SteamTurbineFactory turbineFactory) {
-        this.turbineFactory = turbineFactory;
-    }
 
     public void start() {
         super.start();
