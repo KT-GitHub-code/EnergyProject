85941967a11dd8248d4108d0662f09fa8566995c
KT <krakovszkyt@gmail.com>
2024-04-18 22:40:24 +0200

    add FissionProduct interface

--- src/main/java/com/kt/energyproject/types/powerplants/FissionFragment.java
+++ src/main/java/com/kt/energyproject/types/powerplants/FissionFragment.java
@@ -1,4 +1,4 @@
 package com.kt.energyproject.types.powerplants;
 
-public interface FissionFragment {
+public interface FissionFragment extends FissionProduct {
 }
--- src/main/java/com/kt/energyproject/types/powerplants/Neutron.java
+++ src/main/java/com/kt/energyproject/types/powerplants/Neutron.java
@@ -1,4 +1,4 @@
 package com.kt.energyproject.types.powerplants;
 
-public class Neutron implements SubatomicParticle {
+public class Neutron implements SubatomicParticle, FissionProduct{
 }
