48ff5a0ff89793312bcc9dd80d012596a6bf52b1
KT <krakovszkyt@gmail.com>
2024-04-19 00:12:03 +0200

    add Iodine and Yttrium

---
+++ src/main/java/com/kt/energyproject/types/powerplants/Iodine.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Iodine extends Atom implements FissionFragment {
+
+    public Iodine(int massNumber) {
+        super(53, massNumber);
+    }
+
+}
---
+++ src/main/java/com/kt/energyproject/types/powerplants/Yttrium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Yttrium extends Atom implements FissionFragment {
+
+    public Yttrium(int massNumber) {
+        super(39, massNumber);
+    }
+
+}
