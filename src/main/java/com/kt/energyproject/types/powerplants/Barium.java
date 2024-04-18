710a86842dba8ed2161a4744de2c7357da4ef065
KT <krakovszkyt@gmail.com>
2024-04-18 22:52:34 +0200

    add Krypton and Barium

---
+++ src/main/java/com/kt/energyproject/types/powerplants/Barium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Barium extends Atom implements FissionFragment {
+
+    public Barium(int massNumber) {
+        super(56, massNumber);
+    }
+
+}
---
+++ src/main/java/com/kt/energyproject/types/powerplants/Krypton.java
@@ -0,0 +1,8 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Krypton extends Atom implements FissionFragment {
+
+    public Krypton(int massNumber) {
+        super(36, massNumber);
+    }
+}
