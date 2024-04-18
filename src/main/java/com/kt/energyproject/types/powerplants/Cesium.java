26cd9e006ff18975fcf357a1967d8cafb7abddd4
KT <krakovszkyt@gmail.com>
2024-04-19 00:12:23 +0200

    add Cesium and Rubidium

---
+++ src/main/java/com/kt/energyproject/types/powerplants/Cesium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Cesium extends Atom implements FissionFragment {
+
+    public Cesium(int massNumber) {
+        super(55, massNumber);
+    }
+
+}
---
+++ src/main/java/com/kt/energyproject/types/powerplants/Rubidium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Rubidium extends Atom implements FissionFragment {
+
+    public Rubidium(int massNumber) {
+        super(37, massNumber);
+    }
+
+}
