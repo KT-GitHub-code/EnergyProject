c0463bc1e51cad12889a98ed0a3de96e10885128
KT <krakovszkyt@gmail.com>
2024-04-19 00:12:51 +0200

    add Cerium and Zirconium

---
+++ src/main/java/com/kt/energyproject/types/powerplants/Cerium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Cerium extends Atom implements FissionFragment {
+
+    public Cerium(int massNumber) {
+        super(58, massNumber);
+    }
+
+}
---
+++ src/main/java/com/kt/energyproject/types/powerplants/Zirconium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Zirconium extends Atom implements FissionFragment {
+
+    public Zirconium(int massNumber) {
+        super(40, massNumber);
+    }
+
+}
