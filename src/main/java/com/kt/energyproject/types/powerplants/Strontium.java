627a78c6b3077936d45eee66e1c9da85710cf2bb
KT <krakovszkyt@gmail.com>
2024-04-19 00:11:41 +0200

    add Strontium and Xenon

---
+++ src/main/java/com/kt/energyproject/types/powerplants/Strontium.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Strontium extends Atom implements FissionFragment {
+
+    public Strontium(int massNumber) {
+        super(38, massNumber);
+    }
+
+}
---
+++ src/main/java/com/kt/energyproject/types/powerplants/Xenon.java
@@ -0,0 +1,9 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Xenon extends Atom implements FissionFragment {
+
+    public Xenon(int massNumber) {
+        super(54, massNumber);
+    }
+
+}
