46f361b6d3fd61e19fdabf372fa40c34ac855cd4
KT <krakovszkyt@gmail.com>
2024-04-21 20:49:05 +0200

    implement PWRFuel and ControlRods connection

--- src/main/java/com/kt/energyproject/types/powerplants/PWRFuel.java
+++ src/main/java/com/kt/energyproject/types/powerplants/PWRFuel.java
@@ -6,6 +6,8 @@ public class PWRFuel extends NuclearFuel {
 
     private final Set<Atom> atoms;
 
+    private PressurizedWaterReactorCore reactorCore;
+
     public PWRFuel() {
 
         this.atoms = new HashSet<>();
@@ -20,7 +22,15 @@ public class PWRFuel extends NuclearFuel {
 
     }
 
+    public void setNuclearReactorCore(PressurizedWaterReactorCore reactorCore) {
+        this.reactorCore = reactorCore;
+    }
+
     public void shootNeutron(Neutron neutron) {
+
+        if(neutronGetsAbsorbedByControlRods(neutron)) {
+            return;
+        }
 
         Atom atom = getRandomAtom();
 
@@ -56,6 +66,16 @@ public class PWRFuel extends NuclearFuel {
             iterator.next();
         }
         return iterator.next();
+    }
+
+    private boolean neutronGetsAbsorbedByControlRods(Neutron neutron) {
+        ControlRods controlRods = reactorCore.getControlRods();
+        int positionPercentage = controlRods.getPosition();
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        return randomNumber < positionPercentage;
     }
 
 }
