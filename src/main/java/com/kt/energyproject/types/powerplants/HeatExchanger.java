b07ee30d8b2a3db938004c0175c9c02294f6e865
KT <krakovszkyt@gmail.com>
2024-04-25 21:49:22 +0200

    implement HeatExchanger and Turbine connection

--- src/main/java/com/kt/energyproject/types/powerplants/HeatExchanger.java
+++ src/main/java/com/kt/energyproject/types/powerplants/HeatExchanger.java
@@ -1,13 +1,27 @@
 package com.kt.energyproject.types.powerplants;
 
+import com.kt.energyproject.common.SpinSpeedLevel;
+import com.kt.energyproject.types.turbines.SteamTurbine;
+
 public class HeatExchanger {
+
+    private SteamTurbine turbine;
+
+    public SteamTurbine getTurbine() {
+        return turbine;
+    }
+
+    public void setTurbine(SteamTurbine turbine) {
+        this.turbine = turbine;
+    }
 
     public void absorbHeatFromCoolant() {
         generateSteam();
     }
 
     private void generateSteam() {
-
+        turbine.setSpinSpeed(SpinSpeedLevel.HIGH);
+        turbine.turn();
     }
 
 }
