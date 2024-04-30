eeca812a3bce6a6dc6995e3eb7638d52f0158ee2
KT <krakovszkyt@gmail.com>
2024-04-30 21:52:33 +0200

    add FissionFragmentPair enum

---
+++ src/main/java/com/kt/energyproject/types/powerplants/FissionFragmentPair.java
@@ -0,0 +1,14 @@
+package com.kt.energyproject.types.powerplants;
+
+public enum FissionFragmentPair {
+
+    KRYPTON_BARIUM,
+    STRONTIUM_ZIRCONIUM,
+    CESIUM_XENON,
+    IODINE_RUBIDIUM,
+    XENON_MOLYBDENUM,
+    IODINE_STRONTIUM,
+    XENON_ZIRCONIUM,
+
+}
+
