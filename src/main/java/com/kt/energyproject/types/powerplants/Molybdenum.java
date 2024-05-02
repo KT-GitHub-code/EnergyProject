6f79e15057532213e5d668916f88a903bf2e8bf1
KT <krakovszkyt@gmail.com>
2024-05-02 19:01:30 +0200

    add Molybdenum

---
+++ src/main/java/com/kt/energyproject/types/powerplants/Molybdenum.java
@@ -0,0 +1,8 @@
+package com.kt.energyproject.types.powerplants;
+
+public class Molybdenum extends Atom implements FissionFragment{
+
+    public Molybdenum(int massNumber) {
+        super(42, massNumber);
+    }
+}
