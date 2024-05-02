135dad32af0920dcc88c3f66f4ecb7f26ab87e72
KT <krakovszkyt@gmail.com>
2024-05-02 19:02:24 +0200

    remove unused fission fragments

--- src/main/java/com/kt/energyproject/types/powerplants/Cerium.java
+++
@@ -1,9 +0,0 @@
-package com.kt.energyproject.types.powerplants;
-
-public class Cerium extends Atom implements FissionFragment {
-
-    public Cerium(int massNumber) {
-        super(58, massNumber);
-    }
-
-}
--- src/main/java/com/kt/energyproject/types/powerplants/Yttrium.java
+++
@@ -1,9 +0,0 @@
-package com.kt.energyproject.types.powerplants;
-
-public class Yttrium extends Atom implements FissionFragment {
-
-    public Yttrium(int massNumber) {
-        super(39, massNumber);
-    }
-
-}
